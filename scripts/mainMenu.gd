4f27a00fd5e1827cc7d71945c36b6dc134e4f938
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2025-02-21 22:27:18 +0700

    *: Refactor project structure
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- scripts/mainMenu.gd
+++ scripts/mainMenu.gd
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 func _on_options_pressed() -> void:
 	vbox_container.visible = false
