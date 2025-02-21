e255f03fcb54c4e87498993e7fd47453ba0db5b6
Muhammad Daffa Arrizki Yanma <dappayanma@gmail.com>
2025-02-21 19:25:38 +0700

    Main_menu_3
    Perbaikan dipenamaan

--- mainMenu.gd
+++ mainMenu.gd
@@ -5,36 +5,29 @@ extends Control
 @onready var options_menu = $Options_Menu as OptionsMenu
 @onready var vbox_container = $VBoxContainer as VBoxContainer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	handle_connecting_signals()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	handle_connecting_signals()
-
+	pass
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
-
 
 func _on_options_pressed() -> void:
 	vbox_container.visible = false
 	options_menu.set_process(true)
 	options_menu.visible = true
 
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-	
+
 func on_exit_options_menu() -> void:
 	vbox_container.visible = true
 	options_menu.visible = false
-	
 
-	
 func handle_connecting_signals() -> void:
 	options_button.button_down.connect(_on_options_pressed)
 	options_menu.exit_options_menu.connect(on_exit_options_menu)
--- optionsMenu.gd
+++ optionsMenu.gd
@@ -1,5 +1,6 @@
 class_name OptionsMenu
 extends Control
+
 @onready var exit_button = $MarginContainer/VBoxContainer/Exit_Button as Button
 signal exit_options_menu
 
@@ -7,12 +8,6 @@ signal exit_options_menu
 func _ready() -> void:
 	exit_button.button_down.connect(_on_exit_button_pressed)
 	set_process(false)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#exit_button.button_down.connect(_on_exit_button_pressed)
-
 
 func _on_exit_button_pressed() -> void:
 	exit_options_menu.emit()
