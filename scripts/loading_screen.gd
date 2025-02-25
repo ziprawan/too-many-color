a0bb7d4f4954150e3ef1c3b55ed14692a002e17a
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2025-02-25 17:59:21 +0700

    loading: Round the loading percent number
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- scripts/loading_screen.gd
+++ scripts/loading_screen.gd
@@ -13,7 +13,7 @@ func _process(_delta: float) -> void:
   var progress = []
   ResourceLoader.load_threaded_get_status(WORLD_SCENE_PATH, progress)
   
-  loading_label.text = "Loading... (" + str(progress[0] * 100) + "%)"
+  loading_label.text = "Loading... (" + str(int(progress[0] * 100)) + "%)"
 
   if progress[0] == 1:
     get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(WORLD_SCENE_PATH))
